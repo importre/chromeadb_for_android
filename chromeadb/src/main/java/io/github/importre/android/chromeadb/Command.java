2ffc2b5d8d65196de6c884d3f69b88cddeed456a
importre <importre@importre-ui-MacBook-Air.local>
2014-02-03 00:32:07 +0900

    Added LICENSE file and comments

--- chromeadb/src/main/java/io/github/importre/android/chromeadb/Command.java
+++ chromeadb/src/main/java/io/github/importre/android/chromeadb/Command.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2014, importre. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package io.github.importre.android.chromeadb;
 
 import android.text.TextUtils;
